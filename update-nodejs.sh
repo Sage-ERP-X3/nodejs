a9ac7991b640275f9fb0426235058c34aa1b5541
Eric Boureau <eric.boureau@sage.com>
2016-05-13 17:43:37 +0200

    issue #6 - added 2nd param for el6 version

--- update-nodejs.sh
+++ update-nodejs.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 # Currently the remote branch MUST exist
-# Sample usage: update-nodejs.sh 4.4.4
+# Sample usage: update-nodejs.sh 4.4.4 0.12.14
 git fetch origin
 git checkout $1
 cd win32_x64
@@ -16,6 +16,18 @@ mv node-v$1-linux-x64/bin/node node
 rm -r node-v$1-linux-x64
 rm node-v$1-linux-x64.tar.gz
 
+if [ -n "$2" ]; then
+	cd el6	
+	curl -O https://nodejs.org/dist/v$2/node-v$2-linux-x64.tar.gz
+	tar -xvf node-v$2-linux-x64.tar.gz node-v$2-linux-x64/LICENSE
+	tar -xvf node-v$2-linux-x64.tar.gz node-v$2-linux-x64/bin/node
+	mv node-v$2-linux-x64/LICENSE LICENSE
+	mv node-v$2-linux-x64/bin/node node
+	rm -r node-v$2-linux-x64
+	rm node-v$2-linux-x64.tar.gz
+	cd ..
+fi
+
 cd ../darwin_x64
 curl -O https://nodejs.org/dist/v$1/node-v$1-darwin-x64.tar.gz
 tar -xvf node-v$1-darwin-x64.tar.gz node-v$1-darwin-x64/LICENSE
@@ -25,3 +37,4 @@ mv node-v$1-darwin-x64/bin/node node
 rm -r node-v$1-darwin-x64
 rm node-v$1-darwin-x64.tar.gz
 cd ..
+
