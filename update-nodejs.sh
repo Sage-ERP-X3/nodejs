f407ba93e9136ae7aa7bc665daad5298adbc766f
Eric Boureau <eric.boureau@sage.com>
2016-05-13 16:31:51 +0200

    issue #6 - update script

---
+++ update-nodejs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Currently the remote branch MUST exist
+# Sample usage: update-nodejs.sh 4.4.4
+git fetch origin
+git checkout $1
+cd win32_x64
+curl -O https://raw.githubusercontent.com/nodejs/node/master/LICENSE
+curl -O https://nodejs.org/dist/v$1/win-x64/node.exe
+
+cd ../linux_x64
+curl -O https://nodejs.org/dist/v$1/node-v$1-linux-x64.tar.gz
+tar -xvf node-v$1-linux-x64.tar.gz node-v$1-linux-x64/LICENSE
+tar -xvf node-v$1-linux-x64.tar.gz node-v$1-linux-x64/bin/node
+mv node-v$1-linux-x64/LICENSE LICENSE
+mv node-v$1-linux-x64/bin/node node
+rm -r node-v$1-linux-x64
+rm node-v$1-linux-x64.tar.gz
+
+cd ../darwin_x64
+curl -O https://nodejs.org/dist/v$1/node-v$1-darwin-x64.tar.gz
+tar -xvf node-v$1-darwin-x64.tar.gz node-v$1-darwin-x64/LICENSE
+tar -xvf node-v$1-darwin-x64.tar.gz node-v$1-darwin-x64/bin/node
+mv node-v$1-darwin-x64/LICENSE LICENSE
+mv node-v$1-darwin-x64/bin/node node
+rm -r node-v$1-darwin-x64
+rm node-v$1-darwin-x64.tar.gz
+cd ..
